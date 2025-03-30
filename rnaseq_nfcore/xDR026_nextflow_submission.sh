dce6f6c6e3f62b652e36308da3c6e1fbcd3d53a9
Davide Recchia <daviderecchia@Davides-MacBook-Pro.local>
2025-03-30 14:11:49 +0200

    updated to make it more general

--- rnaseq_nfcore/xDR026_nextflow_submission.sh
+++ rnaseq_nfcore/xDR026_nextflow_submission.sh
@@ -8,32 +8,26 @@
 #SBATCH --error=nfcore_rnaseq_%j.err
 
 
-##don't always have to initialize conda
-## Initialize conda
-source /hpc/local/CentOS7/uu_epigenetics/SOFT/miniconda3/etc/profile.d/conda.sh
-
 ## Activate the nextflow environment
-conda activate /hpc/local/CentOS7/uu_epigenetics/SOFT/miniconda3/envs/nextflow
+source activate /hpc/local/CentOS7/uu_epigenetics/SOFT/miniconda3/envs/nextflow
+
+## Make new CACHEDIR and TMPDIR for Singularity in the project dir.
+## By default, these temporary dirs will be written to the compute node, but there probably isn't enough space for them there.
+## Rather than putting these tmp dirs in the project dir, they could also be written to scratch or one of the global tmp dirs on the hpc.
+mkdir -p ./tmp/singularity_cache
+mkdir -p ./tmp/singularity_temp
+export SINGULARITY_CACHEDIR=./tmp/singularity_cache
+export SINGULARITY_TMPDIR=./tmp/singularity_temp
 
 
-##the cache was getting full, and wass using my home directory as the cache. I'm changing this to use some directory in the working dir. This will have to be deleted later!
-mkdir -p /hpc/shared/uu_epigenetics/davide/rna_seq/xDR026/tmp/singularity_cache
-mkdir -p /hpc/shared/uu_epigenetics/davide/rna_seq/xDR026/tmp/singularity_temp
-export SINGULARITY_CACHEDIR=/hpc/shared/uu_epigenetics/davide/rna_seq/xDR026/tmp/singularity_cache
-export SINGULARITY_TMPDIR=/hpc/shared/uu_epigenetics/davide/rna_seq/xDR026/tmp/singularity_temp
-
-##this is a temporyr fix. thi scan be made permanent by updating my bashrc:
-##echo "export SINGULARITY_CACHEDIR=/hpc/shared/uu_epigenetics/davide/rna_seq/xDR026/tmp/singularity_cache" >> ~/.bashrc
-##echo "export SINGULARITY_TMPDIR=/hpc/shared/uu_epigenetics/davide/rna_seq/xDR026/tmp/singularity_temp" >> ~/.bashrc
-##I wont update this yet, instead I'll wait until I know where to save the temporary files in the scratch.
-##this tmp directory should be manually cleaned when the job is finished running
-
+## Run nf-core rnaseq pipeline
+## Can resume a run by adding the "resume"
 nextflow run nf-core/rnaseq \
 -r 3.12.0 \
 -profile singularity \
---outdir /hpc/shared/uu_epigenetics/davide/rna_seq/xDR026/nextflow_output \
--c /hpc/shared/uu_epigenetics/davide/rna_seq/xDR026/resources.rnaseq.config  \
---input /hpc/shared/uu_epigenetics/davide/rna_seq/xDR026/sample_sheet_xDR026.csv \
+--outdir ./nextflow_output \
+-c ./resources.rnaseq.config  \
+--input ./sample_sheet_xDR026.csv \
 --trimmer trimgalore \
 --aligner star_salmon  \
 --fasta /hpc/shared/uu_epigenetics/davide/annotations/aws_iGenomes/references/Mus_musculus/UCSC/mm10/Sequence/WholeGenomeFasta/genome.fa \
